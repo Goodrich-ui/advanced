79845d26958a2f418342621c32e523628fe9f99c
Goodrich-ui <kelseygoodrich@hotmail.com>
2026-04-06 13:23:31 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -3,22 +3,18 @@ import sys
 
 sys.path.insert(0, os.path.abspath('.'))
 
-# Project info
 project = 'IObit Advanced'
 author = 'Adele Collins'
 release = '1.0'
 
-# General config
-extensions = []
+extensions = ['sphinx_rtd_theme']
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-# HTML settings
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
-# ✅ Google Verification (WORKING METHOD)
-html_context = {
-    google-site-verification" content="nkfFWEaHXct0Lyzc10Hl3Dmo91U7cTPvV0aSmyH4PaA
+html_meta = {
+    "google-site-verification": "nkfFWEaHXct0Lyzc10Hl3Dmo91U7cTPvV0aSmyH4PaA"
 }
